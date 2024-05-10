6c6403184836d34cae65be0d95164bc5f5c33f1f
ibnu-sodik <ibnusodik049@gmail.com>
2024-05-11 01:03:59 +0700

    Handling API Errors

---
+++ src/views/HomeView.vue
@@ -0,0 +1,73 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        v-model="searchQuery"
+        @input="getSearchResults"
+        type="text"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        v-if="mapboxSearchResults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <p v-if="searchError">Sorry, something went wrong, please try again</p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            <!-- V5 -->
+            <!-- {{ searchResult.place_name }} -->
+            <!-- V6 -->
+            {{ searchResult.properties.full_address }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const mapboxAPIKey =
+  "pk.eyJ1IjoiaWJudS1zb2RpayIsImEiOiJjbHcweDdwb2cwNW1rMmpudmtlODZhcTRtIn0.u-iJiu2sUaqQzd49nXw6BQ";
+const searchQuery = ref("");
+const queryTimout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimout.value);
+  queryTimout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        // V5
+        // const resultV5 = await axios.get(
+        //   `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        // );
+        // mapboxSearchResults.value = resultV5.data.features;
+
+        // V6
+        const resultV6 = await axios.get(
+          `https://api.mapbox.com/search/geocode/v6/forward?place=${searchQuery.value}&access_token=${mapboxAPIKey}`
+        );
+        mapboxSearchResults.value = resultV6.data.features;
+      } catch {
+        searchError.value = true;
+      }
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 3000);
+};
+</script>
+
+<style lang="scss" scoped></style>

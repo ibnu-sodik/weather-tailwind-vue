bd1fb4bbe8616af87f3dbf6da700986ebad5b4f1
ibnu-sodik <ibnusodik049@gmail.com>
2024-05-11 10:41:32 +0700

    City View Layout

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,5 +1,106 @@
 <template>
-  <div></div>
+  <div class="flex flex-col flex-1 items-center">
+    <!-- Banner -->
+    <div
+      v-if="route.query.preview"
+      class="text-white p-4 bg-weather-secondary w-full text-center"
+    >
+      <p>
+        Anda sedang melihat pratinjau kota ini, klik ikon "+" untuk memulai
+        melacak kota ini.
+      </p>
+    </div>
+    <!-- Weather Overview -->
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString("id-ID", {
+            weekday: "long",
+            day: "2-digit",
+            month: "long",
+          })
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString("id-ID", {
+            timeStyle: "short",
+          })
+        }}
+      </p>
+      <p class="text-8xl mb-8">
+        {{ Math.round(weatherData.current.temp) }}&deg;
+      </p>
+      <p>
+        Terasa seperti {{ Math.round(weatherData.current.feels_like) }}&deg;
+      </p>
+      <p class="capitaliz">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+      <img
+        :src="`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+        class="w-[150px] h-auto"
+      />
+    </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+    <!-- Hourly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Cuaca Setiap Jam</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div
+            v-for="hourData in weatherData.hourly"
+            :key="hourData.dt"
+            class="flex flex-col gap-4 items-center"
+          >
+            <p class="whitespace-nowrap text-md">
+              {{
+                new Date(hourData.currentTime).toLocaleTimeString("id-ID", {
+                  hour: "numeric",
+                })
+              }}
+            </p>
+            <img
+              class="w-auto h-[50px] object-cover"
+              :src="`https://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+              alt=""
+            />
+            <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+    <!-- Weeklu Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Prakiraan 7 Hari</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString("id-ID", {
+                weekday: "long",
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt=""
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.temp.max) }}</p>
+            <p>L: {{ Math.round(day.temp.min) }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -16,7 +117,7 @@ const getWeatherData = async () => {
 
     // call current date & time
     const localOffset = new Date().getTimezoneOffset() * 60000;
-    const utc = weatherData.data.current.dt * 100 + localOffset;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
     weatherData.data.currentTime =
       utc + 1000 * weatherData.data.timezone_offset;
 
@@ -26,13 +127,13 @@ const getWeatherData = async () => {
       hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
     });
 
-    return weatherData;
+    return weatherData.data;
   } catch (err) {
     console.log(err);
   }
 };
 const weatherData = await getWeatherData();
-console.log(weatherData);
+console.log("weatherData: ", weatherData);
 </script>
 
 <style lang="scss" scoped></style>
